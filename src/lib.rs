366fe111fbe70f2959e16d99769ba26e780aedc0
inesvar <90340779+inesvar@users.noreply.github.com>
2026-01-17 13:49:27 +0100

    Writing code to pass the test (part 1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!();
+    for line in contents.lines() {
+        if line.contains(query) {
+            return vec![line];
+        }
+    }
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn another_one_result() {
+        let query = "Rust";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["Rust:"], search(query, contents));
+    }
 }
