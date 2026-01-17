9dc8eb0dfead28345bc0cc451917afabb2703e23
inesvar <90340779+inesvar@users.noreply.github.com>
2026-01-17 13:10:54 +0100

    Reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() -> Result<(), String> {
     let args: Vec<String> = env::args().collect();
@@ -6,8 +7,17 @@ fn main() -> Result<(), String> {
     let Some([_, pattern, filename]) = args.first_chunk::<3>() else {
         return Err(get_help_message(&args));
     };
-
     println!("Searching '{pattern}' in '{filename}'.");
+
+    let text = match fs::read_to_string(filename) {
+        Ok(result) => result,
+        Err(error) => return Err(error.to_string()),
+    };
+    println!(
+        "{filename} first line: {:?}",
+        text.lines().next().unwrap_or_default()
+    );
+
     Ok(())
 }
 
